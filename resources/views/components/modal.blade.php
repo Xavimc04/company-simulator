9907375537677138a518cb76956b775ac6b734e6
Xavimc04 <xaviermorcam@campus.monlau.com>
2024-01-09 16:40:21 +0000

    Removed shadow from modals

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -16,7 +16,7 @@
         <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
     </div>
 
-    <div x-show="show" class="mb-6 bg-white p-5 rounded-lg overflow-hidden shadow-xl transform transition-all sm:w-full max-w-[500px] sm:mx-auto {{ $styles ?? false ? $styles : '' }}"
+    <div x-show="show" class="mb-6 bg-white p-5 rounded-lg overflow-hidden transform transition-all sm:w-full max-w-[500px] sm:mx-auto {{ $styles ?? false ? $styles : '' }}"
         x-trap.inert.noscroll="show"
         x-transition:enter="ease-out duration-300"
         x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
