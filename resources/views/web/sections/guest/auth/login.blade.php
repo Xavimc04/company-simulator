1f4b1955abd769a12302b4ac6f07606fe013e196
Xavimc04 <xaviermorcam@campus.monlau.com>
2024-01-11 20:15:07 +0000

    Mobile design on Authorization

--- resources/views/web/sections/guest/auth/login.blade.php
+++ resources/views/web/sections/guest/auth/login.blade.php
@@ -5,7 +5,7 @@
         class="flex flex-col items-center justify-center h-screen w-screen bg-no-repeat bg-center bg-cover"
         style="background-image: url({{ URL::to('/') }}/assets/squares-wallpaper.png)"
     >
-        <section class="w-[400px] flex flex-col gap-3 bg-white rounded">
+        <section class="w-[90%] md:w-[400px] flex flex-col gap-3 bg-white rounded">
             <h1 class="text-4xl font-extrabold">Iniciar sesión</h1>
             <p class="text-gray-400 mb-5">Por favor, introduzca sus credenciales para iniciar sesión en la plataforma.</p>
         
