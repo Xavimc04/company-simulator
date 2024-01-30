0f34c35634337bf614832f4c21443fc8ce201221
Xavimc04 <xaviermorcam@campus.monlau.com>
2024-01-30 15:14:53 +0000

    Selector revert

--- resources/views/components/selector.blade.php
+++ resources/views/components/selector.blade.php
@@ -1,12 +1,12 @@
-<div class="flex flex-col gap-2">
+<div class="flex flex-col gap-2 {{ $styles ?? false ? $styles : '' }}">
     @if (isset($label))
         <label class="text-sm text-gray-500">{{ $label }}</label>
     @endif
 
-    <div class="flex items-center bg-white gap-3 border border-black transition-all w-full flex-1 rounded px-3 {{ $styles ?? false ? $styles : '' }}">
+    <div class="flex items-center bg-white gap-3 border border-black transition-all w-full flex-1 rounded px-3">
         <x-icon label="expand_more" />
     
-        <select wire:model.live="{{ $wireModel }}" class="flex-1 py-2.5 bg-transparent text-black appearance-none">
+        <select wire:model.live="{{ $wireModel }}" class="flex-1 py-2.5 bg-transparent text-black">
             <option value=""></option>
 
             @foreach ($options as $value)
