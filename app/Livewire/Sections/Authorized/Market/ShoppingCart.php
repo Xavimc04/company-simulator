d5a3572d2e84549e565c2cadbe2f5d40a076bf4c
Xavimc04 <xaviermorcam@campus.monlau.com>
2024-02-06 15:09:37 +0000

    Shopping cart switch elements and payment information view

--- app/Livewire/Sections/Authorized/Market/ShoppingCart.php
+++ app/Livewire/Sections/Authorized/Market/ShoppingCart.php
@@ -5,13 +5,40 @@ use Livewire\Component;
 use App\Models\CartProduct; 
 use App\Models\Product;
 use App\Models\Company; 
+use Livewire\Attributes\Url;
 
 class ShoppingCart extends Component {
-    protected $items = []; 
+    protected $items = [], $companies = []; 
 
+    public function deleteElement($id) {
+        CartProduct::where('id', $id)->delete();
+    }
+
+    public function addOne($id) {
+        $product = CartProduct::where('id', $id)->first();
+        $product->amount += 1;
+        $product->save();
+    }
+
+    public function removeOne($id) {
+        $product = CartProduct::where('id', $id)->first();
+
+        if(($product->amount - 1) == 0) {
+            $product->delete();
+            return;
+        }
+
+        $product->amount -= 1;
+        $product->save();
+    }
+    
     public function render() {
         $this->items = CartProduct::where('user_id', auth()->user()->id)->get();
-
+    
+        $productIds = CartProduct::where('user_id', auth()->user()->id)->select('product_id')->pluck('product_id')->toArray(); 
+        
+        $this->companies = Company::whereIn('id', Product::whereIn('id', $productIds)->select('company_id')->pluck('company_id')->toArray())->get();
+        
         return view('livewire.sections.authorized.market.shopping-cart');
     }
 }
--- resources/views/livewire/sections/authorized/market/shopping-cart.blade.php
+++ resources/views/livewire/sections/authorized/market/shopping-cart.blade.php
@@ -1,44 +1,133 @@
-<div class="flex-1 p-5">
-    {{-- @ Cart items --}}
-    <section class="flex flex-col divide-y px-5 py-1 rounded shadow-sm bg-white w-[900px]">
-        @foreach ($this->items as $item)
-            <div class="flex items-center rounded h-[70px] gap-3">
-                <div class="w-10 flex items-center justify-center">
-                    @if ($item->product->image)
-                        <img class="rounded-sm h-[30px]" src="{{ asset('storage/companies/' . $item->product['company_id'] . '/products/' . $item->product['image']) }}" />
-                    @else 
-                        <span class="material-symbols-outlined text-md text-blue-500">
-                            fullscreen
-                        </span>
+@php
+    use App\Models\Company; 
+@endphp
+
+<div class="flex-1 gap-7 flex overflow-y-scroll">
+    {{-- @ Companies  --}}
+    <section class="flex flex-col gap-7 p-7 w-2/3 h-full overflow-y-scroll">
+        @foreach ($this->companies as $company)
+            <div class="flex flex-col gap-5 [&:not(:first-child)]:pt-5">
+                {{-- @ Company information --}}
+                <section class="flex items-center gap-3">
+                    @if ($company->icon)
+                        <img class="max-w-[30px] rounded-sm h-[15px]" src="{{ asset('storage/companies/' . $company->icon) }}" />
                     @endif
-                </div>
 
-                <h2 class="text-lg">
-                    {{ $item->product->label }}
-                </h2> 
-                
-                <span class="text-xs text-gray-400 flex-1">#{{ $item->product->reference }}</span>
+                    {{ $company->name }}
+                </section>
 
-                <p class="text-xl font-bold text-blue-500 px-5 text-end">
-                    {{ $item->product->price * $item->amount }} €
-                </p>
+                {{-- @ Cart items --}}
+                <section class="flex flex-col divide-y px-5 py-1 rounded shadow-sm bg-white">
+                    @foreach ($this->items as $item)
+                        @if ($item->product->company_id != $company->id)
+                            @continue
+                        @endif
 
-                <div class="flex items-center gap-4">
-                    <span class="material-symbols-outlined border rounded-full p-2 text-sm px-2.5 select-none cursor-pointer">
-                        remove
-                    </span>
+                        <div class="flex items-center rounded h-[70px] gap-3">
+                            <div class="w-10 flex items-center justify-center">
+                                @if ($item->product->image)
+                                    <img class="rounded-sm h-[30px]" src="{{ asset('storage/companies/' . $item->product['company_id'] . '/products/' . $item->product['image']) }}" />
+                                @else 
+                                    <span class="material-symbols-outlined text-md text-blue-500">
+                                        fullscreen
+                                    </span>
+                                @endif
+                            </div>
+            
+                            <h2 class="text-lg">
+                                {{ $item->product->label }}
+                            </h2> 
+                            
+                            <span class="text-xs text-gray-400 flex-1">#{{ $item->product->reference }}</span>
+            
+                            <p class="text-xl font-bold text-blue-500 px-5 text-end">
+                                {{ $item->product->price * $item->amount }} €
+                            </p>
+            
+                            <div class="flex items-center gap-4">
+                                <span wire:click="removeOne({{ $item->id }})" class="material-symbols-outlined border rounded-full p-2 text-sm px-2.5 select-none cursor-pointer">
+                                    remove
+                                </span>
+            
+                                {{ $item->amount }}
+            
+                                <span wire:click="addOne({{ $item->id }})" class="material-symbols-outlined border rounded-full p-2 text-sm px-2.5 select-none cursor-pointer">
+                                    add
+                                </span>
+                            </div>
+            
+                            <button wire:click="deleteElement({{ $item->id }})" class="material-symbols-outlined text-md opacity-20">
+                                delete
+                            </button>
+                        </div>
+                    @endforeach
+                </section>
 
-                    {{ $item->amount }}
-
-                    <span class="material-symbols-outlined border rounded-full p-2 text-sm px-2.5 select-none cursor-pointer">
-                        add
-                    </span>
-                </div>
-
-                <button class="material-symbols-outlined text-md opacity-20">
-                    delete
-                </button>
+                {{-- @ Proceed to payment --}}
             </div>
         @endforeach
+    </section>
+
+    {{-- @ Subtotal and information --}}
+    <section class="bg-white w-1/3 shadow-md p-5">
+        <h2 class="text-2xl text-blue-500 font-bold">
+            Información del pago
+        </h2>
+
+        <small class="text-gray-600">
+            Revisa la información de pago antes de confirmar la compra.
+        </small>
+
+        <div class="bg-gray-100 border mt-7 p-3">
+            Detalles
+        </div>
+
+        <div class="mt-5 flex flex-col gap-5">
+            <x-labeled-input 
+                label="Nombre" 
+                value="{{ Auth::user()->name }}" 
+                type="text"
+                icon="person"
+                disabled="true"
+            />
+
+            <x-labeled-input 
+                label="NIF / CIF" 
+                value="{{ Auth::user()->current_company && Company::find(Auth::user()->current_company)->cif ? Company::find(Auth::user()->current_company)->cif : 'No disponible' }}"
+                type="text"
+                icon="home"
+                disabled="true"
+            />
+
+            <x-labeled-input 
+                label="Domicilio de entrega"  
+                value="{{ Auth::user()->current_company && Company::find(Auth::user()->current_company)->location ? Company::find(Auth::user()->current_company)->location : 'No disponible' }}"
+                type="text"
+                icon="home" 
+                disabled="true"
+            />
+        </div>
+
+        <div class="bg-gray-100 border mt-7 p-3">
+            Totalidad
+        </div>
+
+        <ul class="mt-5 flex flex-col gap-2">
+            <li class="text-end">Elementos: <span class="text-blue-500 text-xl">{{ $this->items->sum('amount') }}</span></li>
+
+            @php
+                $subtotal = 0;
+
+                foreach ($this->items as $key => $value) {
+                    $subtotal += $value->product->price * $value->amount;
+                }
+            @endphp
+
+            <li class="text-end">Subtotal: <span class="text-blue-500 text-xl">{{ $subtotal }} €</span></li>
+        </ul>
+
+        <div class="flex items-center justify-end">
+            <x-button wireClick="completePayment" styles="justify-center mt-5" icon="credit_card" content="Completar el pago" />
+        </div>
     </section>
 </div>
