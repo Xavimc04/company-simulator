a575e84abce746dfa079240255814b3fd81b6b18
Xavimc04 <xaviermorcam@campus.monlau.com>
2024-02-13 17:12:03 +0000

    Back to company action on market

--- resources/views/web/layouts/market.blade.php
+++ resources/views/web/layouts/market.blade.php
@@ -9,8 +9,18 @@
                 <span class="underline underline-offset-2 decoration-4 decoration-blue-400">Market</span>
             </h1>
 
+            @php
+                $userRole = Auth::user()->role->name;
+                $companyUrl = "/"; 
+    
+                if($userRole == 'Administrador') $companyUrl = '/admin/dashboard';
+                elseif($userRole == 'Profesor') $companyUrl = '/teacher/dashboard';
+                elseif($userRole == 'Estudiante') $companyUrl = '/student/select';
+            @endphp
+
             <ul class="flex gap-4 flex-1 select-none">
                 <li class="cursor-pointer hover:text-blue-500 transition-all" onclick="window.location.href = '/market'">Inicio</li>
+                <li class="cursor-pointer hover:text-blue-500 transition-all" onclick="window.location.href = '{{ $companyUrl }}'">Mi empresa</li>
             </ul>
 
             <p>
