1f9ee990b2fedb0e43a9f43f0827d1e4851f3336
Xavimc04 <xaviermorcam@campus.monlau.com>
2024-02-13 15:10:07 +0000

    Manage buy items and process order

--- app/Livewire/Sections/Authorized/Student/Sells/Buy.php
+++ app/Livewire/Sections/Authorized/Student/Sells/Buy.php
@@ -7,7 +7,27 @@ use Livewire\WithPagination;
 use Livewire\Component;
 
 class Buy extends Component {
+    public $managing, $current_order;
+
     protected $orders = []; 
+    
+    public function manage($identifier) { 
+        try {
+            $this->managing = true;
+            $this->current_order = Order::find($identifier);
+        } catch (\Exception $e) {
+            $this->managing = null;
+        }
+    }
+
+    public function confirm() {
+        try {
+            $this->current_order->update(['status' => 'managed']);
+            $this->managing = null;
+        } catch (\Exception $e) {
+            $this->managing = null;
+        }
+    }
 
     public function render() {
         $this->orders = Order::where('buyer_company_id', auth()->user()->current_company)->paginate(15); 
--- resources/views/livewire/sections/authorized/student/sells/buy.blade.php
+++ resources/views/livewire/sections/authorized/student/sells/buy.blade.php
@@ -23,6 +23,9 @@
                 <th scope="col" class="px-6 py-3">
                     Fecha
                 </th>
+                <th scope="col" class="px-6 py-3">
+                    
+                </th>
             </tr> 
         </thead>
 
@@ -62,6 +65,18 @@
                     <td class="py-4 text-ellipsis truncate pl-5">
                         {{ $order->created_at->format('d/m/Y') }}
                     </td>
+                    <td class="flex justify-end py-4 px-5 items-center">
+                        <button  
+                            wire:click="manage('{{ $order->id }}')" 
+                            class="flex cursor-pointer select-none hover:scale-90 transition-all hover:opacity-70 items-center gap-3 border border-blue-500 bg-blue-500/10 text-blue-500 rounded py-1 px-3"
+                        >
+                            <span class="material-symbols-outlined">
+                                confirmation_number
+                            </span>
+
+                            Gestionar
+                        </button>
+                    </td>
                 </tr>
             @endforeach
         </tbody>
@@ -70,4 +85,57 @@
     <div class="mt-5">
         {{ $this->orders->links() }}
     </div>
+
+    {{-- @ Manage single item --}}
+    <x-modal wire:model="managing" styles="flex flex-col gap-3"> 
+        <h2 class="text-lg flex items-center gap-3">
+            @if ($current_order  && $current_order->seller->icon)
+                <img class="max-w-[30px] rounded-sm h-[15px]" src="{{ asset('storage/companies/' . $current_order->seller->icon) }}" />
+            @endif
+
+            {{ $current_order ? $current_order->seller->name : '' }}
+        </h2>
+
+        <div class="mb-4">
+            <span class="text-green-500 bg-green-100 text-xs px-2 py-1 rounded border border-green-500 uppercase">{{ $current_order ? $current_order->serial : '' }}</span>
+        </div>
+
+        <table class="w-full text-sm text-left text-gray-500">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-100">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        Producto
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Cantidad
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Precio
+                    </th>
+                </tr> 
+            </thead>
+    
+            @if ($current_order && count($current_order->products) > 0) 
+                <tbody>
+                    @foreach ($current_order->products as $product)
+                        <tr class="bg-white border-b">
+                            <td class="py-4 text-ellipsis truncate pl-5"> 
+                                {{ Str::limit($product->product->label, $limit = 30, $end = '...') }}
+                            </td>
+                            <td class="py-4 text-ellipsis truncate pl-5">
+                                {{ $product->amount }}
+                            </td>
+                            <td class="py-4 text-ellipsis truncate pl-5"> 
+                                {{ $product->product->price * $product->amount }}€
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            @endif
+        </table>
+        
+        @if ($current_order && $current_order->status == 'pending')
+            <x-button wireClick="confirm" styles="justify-center" content="Confirmar compra" />
+        @endif
+    </x-modal>
 </div>  
